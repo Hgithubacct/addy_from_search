61713ab60712b9b8ab907af7d3966ff8df22cf37
Henry Drescher <66920978+Hgithubacct@users.noreply.github.com>
2021-03-25 23:12:16 -0500

    updated to a fxn

--- search_address.py
+++ search_address.py
@@ -1,9 +1,18 @@
 import certifi
 import ssl
 import geopy.geocoders
+def address_from_query(query,user):
+    '''
+    args:
+        query: string of what to look up address for
+        user: arbitrary user value to represent who is pining the server
+        
+    returns:
+    location: geolocation object address of the search result'''
+    
+    ctx = ssl.create_default_context(cafile=certifi.where()) 
+    geopy.geocoders.options.default_ssl_context = ctx                                                       
 
-ctx = ssl.create_default_context(cafile=certifi.where()) 
-geopy.geocoders.options.default_ssl_context = ctx                                                       
-
-geolocator = geopy.geocoders.Nominatim(user_agent="henry")
-location = geolocator.geocode("sushi in austin")                                                     
+    geolocator = geopy.geocoders.Nominatim(user_agent=user)
+    location = geolocator.geocode(query)  
+    return location                                                   
